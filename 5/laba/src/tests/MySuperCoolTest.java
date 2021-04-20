d716e9d31a66084d660e571c45b9dfac4e2283dc
HusseinHalifaev <HusseinHalifaev@github.com>
2021-04-20 22:38:52 +0300

    commit

--- 5/laba/src/tests/MySuperCoolTest.java
+++ 5/laba/src/tests/MySuperCoolTest.java
@@ -2,6 +2,9 @@ package tests;
 
 import Commands.Invoker;
 import Commands.Receiver;
+import Commands.SpecificCommands.Clear;
+import Commands.SpecificCommands.Help;
+import Commands.SpecificCommands.Info;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,10 +22,13 @@ public class MySuperCoolTest {
 
     @Test
     public void historyTest() {
+        invoker.register("help", new Help(receiver));
+        invoker.register("info", new Info(receiver));
+        invoker.register("clear", new Clear(receiver));
         invoker.executeCommand(new String[]{"clear"});
         invoker.executeCommand(new String[]{"info"});
         invoker.executeCommand(new String[]{"info"});
         invoker.executeCommand(new String[]{"help"});
-        assertArrayEquals(new String[]{"clear", "info", "info", "help"}, invoker.getCommandHistory());
+        assertArrayEquals(new String[]{"help", "clear", "info", "info",  null, null, null}, invoker.getCommandHistory());
     }
 }
