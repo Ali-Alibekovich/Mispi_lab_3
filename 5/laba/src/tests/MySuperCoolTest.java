c9ba679608ad6c8a7b3bb4fcd307b189dbff065f
HusseinHalifaev <HusseinHalifaev@github.com>
2021-04-21 14:18:42 +0300

    commmit

--- 5/laba/src/tests/MySuperCoolTest.java
+++ 5/laba/src/tests/MySuperCoolTest.java
@@ -6,13 +6,10 @@ import Commands.Receiver;
 import Commands.SpecificCommands.Clear;
 import Commands.SpecificCommands.Help;
 import Commands.SpecificCommands.Info;
-import jdk.jfr.Name;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
-@Name("Test1")
 public class MySuperCoolTest {
     static Invoker invoker;
     static Receiver receiver;
